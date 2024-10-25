c10fd3eedeb997aef682a60ea0b6a3b06e6b1f94
Harsh Tiwari <131146410+Master-harsh@users.noreply.github.com>
2024-10-25 17:05:24 +0530

    script.js

--- Project_01/script.js
+++ Project_01/script.js
@@ -30,36 +30,29 @@ buttons.forEach(function () {
                 break;
         
             default:
-                body.style.backgroundColor = "gray";
+                body.style.backgroundColor = "white";
                 break;
         }
+
+        // USING IF/ELSE 
+
+        // if(e.target.id == 'gray'){
+        //     divBox.style.backgroundColor = "gray";
+        //     divBox.style.color = "red";
+        // } else if(e.target.id == 'white'){
+        //     divBox.style.backgroundColor = "white";
+        // } else if(e.target.id == 'yellow'){
+        //     divBox.style.backgroundColor = "yellow";
+        // } else if(e.target.id == 'blue'){
+        //     divBox.style.backgroundColor = "blue";
+        // } else if(e.target.id == 'purple'){
+        //     divBox.style.backgroundColor = "purple";
+        // } else{
+        //     divBox.style.backgroundColor = "black";
+        //     divBox.style.color = "white"
+        // }
+
     });
 });
 
-// buttons.addEventListener("click", function(e){
-//     console.log(e);
 
-//     console.log(e.target);
-    
-//     switch (e.target.id) {
-//         case 'gray':
-//             body.style.backgroundColor = "gray";
-//             break;
-
-//             case 'white':
-//             body.style.backgroundColor = "white";
-//             break;
-
-//             case 'yellow':
-//             body.style.backgroundColor = "yellow";
-//             break;
-
-//             case 'blue':
-//             body.style.backgroundColor = "blue";
-//             break;
-    
-//         default:
-//             body.style.backgroundColor = "gray";
-//             break;
-//     }
-// });
